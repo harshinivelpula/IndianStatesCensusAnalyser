9e87ab643ba0a2d63c6817a71cbc11e8f605ec7d
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-03 15:36:11 +0530

    [charu] Reading data from statecode CSV file

---
+++ IndianStatesCensusAnalyser/CSVStateCode.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    public class CSVStateCode
+    {
+        public void ReadStateCodeData(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<StateCodeData>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data.SrNo+" " +data.StateName+" "+data.TIN+" "+data.StateCode+" ");
+                }
+            }
+        }
+    }
+}
--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -4,11 +4,15 @@ namespace IndianStatesCensusAnalyserProgram
 {
     class Program
     {
-        public static string stateCensusCSVFilePath = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\Files\StateCensusData.csv";        
+        public static string stateCensusCSVFilePath = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\Files\StateCensusData.csv";
+        public static string stateCodeCSVFilePth = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\Files\StateCode.csv";
         public static void Main(string[] args)
         {
             CSVStateCensus cSVStateCensus = new CSVStateCensus();
             cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+
+            CSVStateCode cSVStateCode= new CSVStateCode();
+            cSVStateCode.ReadStateCodeData(stateCodeCSVFilePth);
         }
     }
 }
