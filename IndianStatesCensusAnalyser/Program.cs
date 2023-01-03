b422b1bb53ffdea99bd840853320d4f123e735df
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-03 15:05:44 +0530

    [charu] Reading data from CSV file

---
+++ IndianStatesCensusAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using IndianStatesCensusAnalyser;
+
+namespace IndianStatesCensusAnalyserProgram
+{
+    class Program
+    {
+        public static string stateCensusCSVFilePath = @"C:\Users\DELL\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\Files\StateCensusData.csv";        
+        public static void Main(string[] args)
+        {
+            CSVStateCensus cSVStateCensus = new CSVStateCensus();
+            cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+        }
+    }
+}
